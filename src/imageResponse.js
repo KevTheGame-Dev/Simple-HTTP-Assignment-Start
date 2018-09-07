e0941eb271849ae90669f1086c90eb794bf0adb4
Kevin Barrett <kevincb45@gmail.com>
2018-09-07 10:32:58 -0400

    Completed assignment

---
+++ src/imageResponse.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getMeme = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(index);
+  response.end();
+};
+
+
+module.exports.getMeme = getMeme;
